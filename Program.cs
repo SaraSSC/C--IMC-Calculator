755d93d1c0ec1703202f4cf22464da103737e926
Sattineez_SC <83277661+SaraSSC@users.noreply.github.com>
2024-03-15 02:04:35 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ficha1_Exer3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura;
+            double peso;
+
+
+            Console.WriteLine("Introduza o seu peso (kg): ");
+            peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduza a sua altura (metros): ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+
+            //Math.pow = potências (pow e ** em python)
+
+            altura = Math.Pow(altura, 2);
+            double IMC = (peso / altura);
+
+            // F* delimita as casa decimais apresentada no output
+            Console.WriteLine($"O seu IMC é : {IMC:F2}");
+        }
+    }
+}
